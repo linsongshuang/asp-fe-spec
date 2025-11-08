cc31498a2f7ac0f3438f55ea6de2ad6879ec52c1
linsongshuang_a <linsongshuang_a@aspirecn.com>
2025-11-08 18:40:45 +0800

    feat:commitlint

--- packages/asp-commitlint/lib/asp-commitlint.js
+++ packages/asp-commitlint/lib/asp-commitlint.js
@@ -1,7 +1,15 @@
 'use strict';
 
-module.exports = aspCommitlint;
-
-function aspCommitlint() {
-  return 'Hello from aspCommitlint 222';
+module.exports = {
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build', 'ci', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build', 'ci', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'build', 'ci']],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0],
+    'subject-case': [0],
+    'subject-empty': [0],
+  }
 }
